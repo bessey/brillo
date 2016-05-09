2f9b4c743a42aa7f779e9fbb2b482354f03ffaaa
Matt Bessey <mbessey@caring.com>
2016-05-09 14:28:14 -0700

    [mysql] don't forget the schema migrations table

--- lib/brillo/scrubber.rb
+++ lib/brillo/scrubber.rb
@@ -119,13 +119,14 @@ module Brillo
 
     def adapter_header
       return unless config.db[:adapter] == "mysql2"
-      <<-SQL
-      -- Disable autocommit, uniquechecks, and foreign key checks, for performance on InnoDB
-      -- http://dev.mysql.com/doc/refman/5.5/en/optimizing-innodb-bulk-data-loading.html
-      SET @OLD_AUTOCOMMIT=@@AUTOCOMMIT, AUTOCOMMIT = 0;
-      SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS = 0;
-      SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS = 0;
-      SQL
+      ActiveRecord::Base.connection.dump_schema_information +
+        <<-SQL
+        -- Disable autocommit, uniquechecks, and foreign key checks, for performance on InnoDB
+        -- http://dev.mysql.com/doc/refman/5.5/en/optimizing-innodb-bulk-data-loading.html
+        SET @OLD_AUTOCOMMIT=@@AUTOCOMMIT, AUTOCOMMIT = 0;
+        SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS = 0;
+        SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS = 0;
+        SQL
     end
 
     def adapter_footer
